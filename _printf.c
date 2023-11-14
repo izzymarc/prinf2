a53db98388cc6666de466b951592d799439b7351
ezekielgwamna <izzymarc@gmail.com>
2023-11-14 13:01:04 -0800

    extra

---
+++ _printf.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * _printf - Custom implementation of the printf function
+ * @format: Format string containing the characters and format specifiers
+ * 
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    int count = 0; // Counter for the number of characters printed
+    va_list args;  // To handle the variable number of arguments
+    int i = 0;     // Loop index for iterating through the format string
+    char ch;       // Temporary character for `%c`
+    char *str;     // Temporary string for `%s`
+
+    // Start processing the variable arguments
+    va_start(args, format);
+
+    // Iterate through the format string
+    while (format && format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++; // Move to the next character for specifier
+            switch (format[i])
+            {
+                case 'c': // Character
+                    ch = (char)va_arg(args, int); // Fetch char argument
+                    putchar(ch); // Print the character
+                    count++; // Increment count
+                    break;
+
+                case 's': // String
+                    str = va_arg(args, char*); // Fetch string argument
+                    while (*str) // Print each character of the string
+                    {
+                        putchar(*str);
+                        str++;
+                        count++;
+                    }
+                    break;
+
+                case '%': // Percent sign
+                    putchar('%'); // Print the percent sign
+                    count++; // Increment count
+                    break;
+
+                default: // Unsupported specifiers
+                    // You can handle unsupported specifiers here
+                    break;
+            }
+        }
+        else // Regular character
+        {
+            putchar(format[i]); // Print the regular character
+            count++; // Increment count
+        }
+        i++; // Move to the next character in the format string
+    }
+
+    // Clean up
+    va_end(args);
+
+    return count; // Return the number of characters printed
+}
---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h> /* For variadic functions */
+#include <stdio.h>  /* For standard input/output */
+#include <limits.h> /* For integer limits */
+
+/*
+ * Prototype for your _printf function. This function implements a custom
+ * printf function for formatted output.
+ */
+int _printf(const char *format, ...);
+
+/*
+ * Any additional function prototypes, macros, or type definitions required
+ * by _printf.
+ */
+
+#endif /* MAIN_H */
