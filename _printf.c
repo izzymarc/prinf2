21d206f3a0ca08e3c737bcd18d9b05b8b26a92e3
izzymarc <izzymarc@gmail.com>
2023-11-14 22:05:28 +0100

    pintf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,7 @@
+
 #include "main.h"
 
-/**
+/*
  * _printf - Custom implementation of the printf function
  * @format: Format string containing the characters and format specifiers
  * 
@@ -8,32 +9,37 @@
  */
 int _printf(const char *format, ...)
 {
-    int count = 0; // Counter for the number of characters printed
-    va_list args;  // To handle the variable number of arguments
-    int i = 0;     // Loop index for iterating through the format string
-    char ch;       // Temporary character for `%c`
-    char *str;     // Temporary string for `%s`
+    /* Counter for the number of characters printed */
+    int count = 0;
+    /* To handle the variable number of arguments */
+    va_list args;
+    /* Loop index for iterating through the format string */
+    int i = 0;
+    /* Temporary character for `%c` */
+    char ch;
+    /* Temporary string for `%s` */
+    char *str;
 
-    // Start processing the variable arguments
+    /* Start processing the variable arguments */
     va_start(args, format);
 
-    // Iterate through the format string
+    /* Iterate through the format string */
     while (format && format[i])
     {
         if (format[i] == '%')
         {
-            i++; // Move to the next character for specifier
+            i++; /* Move to the next character for specifier */
             switch (format[i])
             {
-                case 'c': // Character
-                    ch = (char)va_arg(args, int); // Fetch char argument
-                    putchar(ch); // Print the character
-                    count++; // Increment count
+                case 'c': /* Character */
+                    ch = (char)va_arg(args, int); /* Fetch char argument */
+                    putchar(ch); /* Print the character */
+                    count++; /* Increment count */
                     break;
 
-                case 's': // String
-                    str = va_arg(args, char*); // Fetch string argument
-                    while (*str) // Print each character of the string
+                case 's': /* String */
+                    str = va_arg(args, char*); /* Fetch string argument */
+                    while (*str) /* Print each character of the string */
                     {
                         putchar(*str);
                         str++;
@@ -41,26 +47,26 @@ int _printf(const char *format, ...)
                     }
                     break;
 
-                case '%': // Percent sign
-                    putchar('%'); // Print the percent sign
-                    count++; // Increment count
+                case '%': /* Percent sign */
+                    putchar('%'); /* Print the percent sign */
+                    count++; /* Increment count */
                     break;
 
-                default: // Unsupported specifiers
-                    // You can handle unsupported specifiers here
+                default: /* Unsupported specifiers */
+                    /* You can handle unsupported specifiers here */
                     break;
             }
         }
-        else // Regular character
+        else /* Regular character */
         {
-            putchar(format[i]); // Print the regular character
-            count++; // Increment count
+            putchar(format[i]); /* Print the regular character */
+            count++; /* Increment count */
         }
-        i++; // Move to the next character in the format string
+        i++; /* Move to the next character in the format string */
     }
 
-    // Clean up
+    /* Clean up */
     va_end(args);
 
-    return count; // Return the number of characters printed
+    return count; /* Return the number of characters printed */
 }
